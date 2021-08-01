ab36a337f0d7d4af19e345bca92caa7788b11042
Julian Churchill <chewielouie03@gmail.com>
2021-08-01 09:03:04 +0100

    Added guns

--- Assets/Scripts/SingleShotGun.cs
+++ Assets/Scripts/SingleShotGun.cs
@@ -1,24 +1,43 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SingleShotGun : Gun
 {
+	[SerializeField] Camera cam;
 
-    [SerializeField] Camera cam;
+	PhotonView PV;
 
-    public override void Use()
+	void Awake()
+	{
+		PV = GetComponent<PhotonView>();
+	}
+
+	public override void Use()
+	{
+		Shoot();
+	}
+
+	void Shoot()
+	{
+		Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f));
+		ray.origin = cam.transform.position;
+		if(Physics.Raycast(ray, out RaycastHit hit))
+		{
+			hit.collider.gameObject.GetComponent<IDamageable>()?.TakeDamage(((GunInfo)itemInfo).damage);
+			PV.RPC("RPC_Shoot", RpcTarget.All, hit.point);
+		}
+	}
+
+	[PunRPC]
+	void RPC_Shoot(Vector3 hitPosition)
     {
-        Shoot();
-    }
-
-    void Shoot()
-    {
-        Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f));
-        ray.origin = cam.transform.position;
-        if(Physics.Raycast(ray, out RaycastHit hit))
+        Collider[] colliders = Physics.OverlapSphere(hitPosition, 0.3f);
+        if(colliders.Length != 0)
         {
-            hit.collider.gameObject.GetComponent<IDamageable>()?.TakeDamage(((GunInfo)itemInfo).damage);
+            GameObject bulletImpactObj = Instantiate(bulletImpactPrefab, hitPosition, Quaternion.identity);
+            bulletImpactObj.transform.SetParent(colliders[0].transform);
         }
     }
 }
